458b10d47e8f3eb7d56c81c510ab43067bf57fe3
Manmeet Singh <gdm.manmeet@gmail.com>
2015-02-25 10:31:16 +0530

    Revert "ACCOUNT_EMAIL_VERIFICATION = (mandatory)"
    This reverts commit c0c72549c87776d6f653a7846dc167961ad8a4be.

--- teachoo_web_project/settings/production.py
+++ teachoo_web_project/settings/production.py
@@ -28,5 +28,4 @@ ALLOWED_HOSTS = ['*']
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
-#django-allauth email
-ACCOUNT_EMAIL_VERIFICATION = ("mandatory")
+
