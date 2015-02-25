18638d14c9bfb25ae3c07cc2d553f29614baa885
Manmeet Singh <gdm.manmeet@gmail.com>
2015-02-25 09:21:39 +0530

    removed email user and email password from settings.py

--- teachoo_web_project/settings/base.py
+++ teachoo_web_project/settings/base.py
@@ -169,9 +169,7 @@ SECRET_KEY = os.environ["SECRET_KEY"]
 
 #Email
 DEFAULT_FROM_EMAIL="no-reply@teachoo.com"
-# Sendgrid credentials
+EMAIL_BACKEND='django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST=os.environ["EMAIL_HOST"]
-EMAIL_HOST_USER=os.environ["EMAIL_HOST_USER"]
-EMAIL_HOST_PASSWORD = os.environ["EMAIL_HOST_PASSWORD"]
 EMAIL_PORT=int(os.environ["EMAIL_PORT"])
 EMAIL_USE_TLS=os.environ["EMAIL_USE_TLS"]
