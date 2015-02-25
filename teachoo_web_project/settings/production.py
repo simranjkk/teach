c0c72549c87776d6f653a7846dc167961ad8a4be
Manmeet Singh <gdm.manmeet@gmail.com>
2015-02-25 10:13:00 +0530

    ACCOUNT_EMAIL_VERIFICATION = (mandatory)

--- teachoo_web_project/settings/production.py
+++ teachoo_web_project/settings/production.py
@@ -28,4 +28,5 @@ ALLOWED_HOSTS = ['*']
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
-
+#django-allauth email
+ACCOUNT_EMAIL_VERIFICATION = ("mandatory")
