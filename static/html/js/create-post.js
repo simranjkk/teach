b1ac37de9b78797b251fc57bd7117d8a2d03114a
Manmeet Singh <gdm.manmeet@gmail.com>
2015-06-09 16:51:27 +0530

    auto select last used category in create post

---
+++ static/html/js/create-post.js
@@ -0,0 +1,33 @@
+function setCookie(cname, cvalue, exdays) {
+    var d = new Date();
+    d.setTime(d.getTime() + (exdays*24*60*60*1000));
+    var expires = "expires="+d.toUTCString();
+    document.cookie = cname + "=" + cvalue + "; " + expires;
+}
+
+function getCookie(cname) {
+    var name = cname + "=";
+    var ca = document.cookie.split(';');
+    for(var i=0; i<ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0)==' ') 	
+			c = c.substring(1);
+        if (c.indexOf(name) == 0) 
+			return c.substring(name.length, c.length);
+    }
+    return "";
+}
+
+function select_last_used_main_category(){
+	main_category = getCookie( "main_category" );
+	if( main_category )
+		$("#main_categories").val( main_category ).change();
+}
+
+function select_last_used_category(){
+	category = getCookie( "category" );
+	main_category = $("#main_categories").val();
+	main_category_cookie = getCookie( "main_category" )
+	if( category && main_category == main_category_cookie )
+		$("#id_category").val( category );
+}
